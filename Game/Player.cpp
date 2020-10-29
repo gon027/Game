9ab91286be716cb3a0a51dc1d3d7528af1709273
Takashi <gon27.tk08@gmail.com>
2020-10-30 08:08:17 +0900

    ジャンプを少し改良した

--- Game/Player.cpp
+++ Game/Player.cpp
@@ -12,7 +12,7 @@ namespace gnGame {
 		constexpr float Speed = 5.0f;
 
 		// プレイヤーのジャンプ力
-		constexpr float JumpPower = 7.0f;
+		constexpr float JumpPower = -7.0f;
 
 
 		// プレイヤーの進む距離
@@ -38,7 +38,7 @@ namespace gnGame {
 
 	Player::Player()
 		: pImage()
-		, pos(100, 100)
+		, pos()
 		, velocity()
 		, isJump(false)
 		, isGround(false)
@@ -47,71 +47,86 @@ namespace gnGame {
 
 	void Player::onStart()
 	{
+		// -- 座標初期化 --
+		pos.setPos(100, 350);
+
 		bounds.minPos.setPos(0, 0);
 		bounds.maxPos.setPos(32, 64);
 		bounds.size.setPos(bounds.maxPos - bounds.minPos);
 		bounds.center.setPos(bounds.size.half());
 	}
 
+	int frame = 0;
+
 	void Player::onUpdate()
 	{
-		// 移動変数部
-		float gy = PlayerInfo::Gravity;
-
-		// 移動部
-		pos.x += PlayerInfo::getAxis() * PlayerInfo::Speed;
-
+		// ----- 移動 -----
+		velocity.x = PlayerInfo::getAxis() * PlayerInfo::Speed;
+		
+		// ジャンプキーが押された時
 		if (Input::getKeyDown(Key::SPACE)) {
-			//isJump = 1;
-			pos.x = 300.f;
-
+			// 地面に足がついているとき
 			if (isGround) {
+				isJump   = true;
 				isGround = false;
-				isJump = 1;
-				pos.y -= PlayerInfo::JumpPower;
-				isJump = 0;
 			}
 		}
 
+		if (isJump) {
+			++frame;
+			velocity.y = PlayerInfo::JumpPower;
 
-		// 判定ボックス更新部
-		bounds.minPos.setPos(pos.x - bounds.center.x, pos.y - bounds.center.y);
-		bounds.maxPos.setPos(32 + pos.x - bounds.center.x, 64 + pos.y - bounds.center.y);
-		bounds.size.setPos(bounds.maxPos - bounds.minPos);
-		bounds.center.setPos(bounds.size.half());
+			if (frame == 30) {
+				isJump = false;
+				frame = 0;
+			}
+		}
+		else {
+			velocity.y = PlayerInfo::Gravity;
+		}
 
-		// 移動判定部
-		if (pos.y + bounds.center.y >= 416) {
-			gy = 0;
+		// ----- 移動判定部 -----
+
+		// velocityを加算したときの座標
+		auto nextPos = pos + velocity;
+
+		// 判定を行う
+		if (nextPos.y + bounds.center.y >= 416) {
+			velocity.y = 0;
 			isGround = true;
 		}
 
-
-		if (pos.x + bounds.center.x >= WindowInfo::WindowWidth) {
-			pos.x = WindowInfo::WindowWidth - bounds.center.x;
+		if (nextPos.x + bounds.center.x >= WindowInfo::WindowWidth) {
+			velocity.x = 0;
 		}
 
-		if (pos.x - bounds.center.x <= 0) {
-			pos.x = 0 + bounds.center.x;
+		if (nextPos.x - bounds.center.x <= 0) {
+			velocity.x = 0;
 		}
 
-		pos.y += gy;
+		// ----- 座標更新 -----
+		pos += velocity;
 
-		// 座標更新部
+		// 判定ボックス更新
+		bounds.minPos.setPos(pos.x - bounds.center.x, pos.y - bounds.center.y);
+		bounds.maxPos.setPos(32 + pos.x - bounds.center.x, 64 + pos.y - bounds.center.y);
+
 		pImage.sprite.setPos(pos);
 
-
-		// 描画部
+		// ----- 描画 -----
 		pImage.sprite.draw();
 
+		// ----- デバッグ -----
 		debug();
 	}
 
 	void Player::debug()
 	{
-		Debug::drawFormatText(0, 0, Color::Black, "Pos = %s", pos.toString().c_str());
-		Debug::drawFormatText(0, 20, Color::Black, "isGround = %d", isGround);
-		Debug::drawFormatText(0, 40, Color::Black, "isJump   = %d", isJump);
+		Debug::drawFormatText(0, 0,  Color::Black, "Position = %s", pos.toString().c_str());
+		Debug::drawFormatText(0, 20, Color::Black, "Velocity = %s", velocity.toString().c_str());
+		Debug::drawFormatText(0, 40, Color::Black, "isGround = %d", isGround);
+		Debug::drawFormatText(0, 60, Color::Black, "isJump   = %d", isJump);
+		Debug::drawFormatText(0, 80, Color::Black, "frame    = %d", frame);
 
 		Debug::drawLine(bounds.minPos, Vector2{ bounds.minPos.x, bounds.maxPos.y }, 3.f, Color::Red);
 		Debug::drawLine(bounds.minPos, Vector2{ bounds.maxPos.x, bounds.minPos.y }, 3.f, Color::Red);
